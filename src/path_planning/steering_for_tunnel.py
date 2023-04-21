6b593cf385862710328df734e2ff724f114ebfe6
yeonseok <leeys4140@naver.com>
2023-04-21 17:02:45 +0900

    라이다 변경에 따른 list 구간 수정

--- src/path_planning/steering_for_tunnel.py
+++ src/path_planning/steering_for_tunnel.py
@@ -21,15 +21,15 @@ class SteeringInTunnel:
         self.w2 = 0.5
 
     def scan_callback(self, scan):
-        self.sub_scan = scan.ranges     # ranges 하면 모든 값들 다 받아 오는 건가? 그리고 값 들의 해상도 는 얼마나 되지??
+        self.sub_scan = scan.ranges[0:810:3]     # ranges 하면 모든 값들 다 받아 오는 건가? 그리고 값 들의 해상도 는 얼마나 되지??
 
     def get_steer(self):    # scan_data 의 데이터 타입 확인 후 0.0의 데이터 타입 수정 필요
         self.sub_scan[self.sub_scan == 0.0] = self.max_dis  # 최대 측정 거리를 넘어가 값이 0.0 으로 들어올 때 max_dis 로 변환
-        longest_angle = np.argmax(self.sub_scan[300:360:5] + self.sub_scan[0:60 + 1:5])  # 좌우 60° 중 거리가 가장 먼 angle 탐색
+        longest_angle = np.argmax(self.sub_scan[75:195 + 1:5])  # 좌우 60° 중 거리가 가장 먼 angle 탐색
         steer1 = max(min(longest_angle - 60, 22), -22)  # 가장 먼 angle 을 정면 기준 으로 바꿔 steer 값으로 변환
                                                         # -22°를 넘으면 -22°로, 22°를 넘으면 22°
 
-        r_data, l_data = self.sub_scan[260:280 + 1:3], self.sub_scan[80:100 + 1:3]  # 좌우 20° 범위 거리 데이터
+        r_data, l_data = self.sub_scan[35:55 + 1:3], self.sub_scan[215:235 + 1:3]  # 좌우 20° 범위 거리 데이터
         r_avg, l_avg = sum(r_data) / len(self.sub_scan), sum(l_data) / len(self.sub_scan)  # 20° 범위의 값들의 평균
         steer2 = ((l_avg - r_avg) / (self.tunnel_width - self.width)) * 22  # normalization 후 22를 곱하여 steer 값으로 변환
 
@@ -64,8 +64,9 @@ class SteeringInTunnel:
     급격한 코너가 있는 터널의 경우는 Version 2가, 완만한 곡률의 터널인 경우는 Version 1 이 더 성능이 좋을 것이라 생각 한다.
     둘 다 안좋을 수도...?
     '''
-    
-class publish_erp():
+
+
+class PublishToErp:
     def __init__(self):
         self.erp_pub = rospy.Publisher("erp_write", Twist, queue_size=30)
         self.erp = Twist()
@@ -74,18 +75,19 @@ class publish_erp():
         self.erp.linear.x = speed
         self.erp.angular.z = steer
         self.erp_pub.publish(self.erp)
-    
+
+
 def main():
-    #기본 설정
     rospy.init_node('tunnel_node', anonymous=True)
 
-    Tunnel = SteeringInTunnel() # 객체 생성
-    pub = publish_erp()
+    Tunnel = SteeringInTunnel()
+    pub = PublishToErp()
 
     while not rospy.is_shutdown():
         speed = 30
         steer = Tunnel.get_steer()
         pub.pub_erp(speed, steer)
+
 
 if __name__ == '__main__':
     main()
