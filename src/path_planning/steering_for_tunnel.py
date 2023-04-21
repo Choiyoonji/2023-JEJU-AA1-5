8f5b376a5171c4b12dbe6d96af4a74e96b64bf50
choiyoonji <cyj21c6352@gmail.com>
2023-04-21 19:38:29 +0900

    0421 테스트

--- src/path_planning/steering_for_tunnel.py
+++ src/path_planning/steering_for_tunnel.py
@@ -4,13 +4,17 @@ import numpy as np
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
-Version = 2
+import time
+
+Version = 1
 
 class SteeringInTunnel:
     def __init__(self):
         self.laser_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback, queue_size=1)
-        self.sub_scan = []  # np.zeros(360)
-
+        # self.sub_scan = [0]  # np.zeros(360)
+        self.sub_scan = []
+        for i in range(810):
+            self.sub_scan.append(0.0)
         # self.wheel_base = 0.75  # 차축 간 거리 [m]
         self.width = 0.77  # 차 폭 [m]
         # self.length = 1.35  # 차 길이 [m]
@@ -21,23 +25,35 @@ class SteeringInTunnel:
         self.w2 = 0.5
 
     def scan_callback(self, scan):
-        scan = np.array(scan.ranges[0:810:3])     # ranges 하면 모든 값들 다 받아 오는 건가? 그리고 값 들의 해상도 는 얼마나 되지??
-        scan[scan >= self.max_dis or scan == 0.0] = self.max_dis
-        self.sub_scan = scan.tolist()
+        # scan = np.array(scan.ranges[0:810:3])     # ranges 하면 모든 값들 다 받아 오는 건가? 그리고 값 들의 해상도 는 얼마나 되지??
+        # print(scan)
+        
+        # scan[scan >= self.max_dis] = self.max_dis
+        self.sub_scan = list(scan.ranges[0:811:3])
 
     def get_steer(self):    # scan_data 의 데이터 타입 확인 후 0.0의 데이터 타입 수정 필요
-        self.sub_scan[self.sub_scan == 0.0] = self.max_dis  # 최대 측정 거리를 넘어가 값이 0.0 으로 들어올 때 max_dis 로 변환
+        # print("len: ",len(self.sub_scan))
+        self.sub_scan[self.sub_scan == 'inf'] = self.max_dis  # 최대 측정 거리를 넘어가 값이 0.0 으로 들어올 때 max_dis 로 변환
         longest_angle = np.argmax(self.sub_scan[75:195 + 1])  # 좌우 60° 중 거리가 가장 먼 angle 탐색
         steer1 = max(min(longest_angle - 60, 22), -22)  # 가장 먼 angle 을 정면 기준 으로 바꿔 steer 값으로 변환
                                                         # -22°를 넘으면 -22°로, 22°를 넘으면 22°
 
-        r_data, l_data = self.sub_scan[35:55 + 1:3], self.sub_scan[215:235 + 1:3]  # 좌우 20° 범위 거리 데이터
-        r_avg, l_avg = sum(r_data) / len(self.sub_scan), sum(l_data) / len(self.sub_scan)  # 20° 범위의 값들의 평균
-        steer2 = ((l_avg - r_avg) / (self.tunnel_width - self.width)) * 22  # normalization 후 22를 곱하여 steer 값으로 변환
-
+        r_data, l_data = self.sub_scan[55:75 + 1:3], self.sub_scan[195:215 + 1:3]  # 좌우 20° 범위 거리 데이터
+        
+        r_avg, l_avg = sum(r_data) / len(r_data), sum(l_data) / len(l_data)  # 20° 범위의 값들의 평균
+        print('r: ', r_avg)
+        print('l: ', l_avg)
+        # steer2 = ((l_avg - r_avg) / (1.8)) * 22
+        steer2 = ((r_avg - l_avg) / (self.tunnel_width - self.width)) * 22 * 1.1 # normalization 후 22를 곱하여 steer 값으로 변환
+        
+        # print("========================")
+        # print('steer2: ', steer2)
+        # print('steer1: ', steer1)
+        
+        return max(min(steer2, 22), -22)
         if Version == 1:
             return self.w1 * steer1 + self.w2 * steer2
-        elif Version == 2:
+        elif Version == 2: 
             return max(min(steer1 + steer2, 22), -22)
         else:
             pass
@@ -86,8 +102,10 @@ def main():
     pub = PublishToErp()
 
     while not rospy.is_shutdown():
-        speed = 30
+        time.sleep(0.1)
+        speed = 50
         steer = Tunnel.get_steer()
+        print('steer: ',steer)
         pub.pub_erp(speed, steer)
 
 
