5e8d23d6b850e2ae782fb24ba9d6e18db3bed0fc
yeonseok <leeys4140@naver.com>
2023-04-22 22:24:15 +0900

    max_dis 를 넘어가는 값에 대한 처리 수정

--- src/path_planning/steering_for_tunnel.py
+++ src/path_planning/steering_for_tunnel.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+# noinspection PyUnresolvedReferences
 import rospy
 import time
+import numpy as np
+# noinspection PyUnresolvedReferences
 from sensor_msgs.msg import LaserScan
+# noinspection PyUnresolvedReferences
 from geometry_msgs.msg import Twist
 
 
@@ -16,14 +20,16 @@ class SteeringInTunnel:
         self.max_dis = 15.0  # lidar 센서 최대 측정 범위 [m]
 
     def scan_callback(self, scan):
-        self.sub_scan = list(scan.ranges[0:811:3])
+        # self.sub_scan = list(scan.ranges[0:810+1:3])  # 0° ~ 270° 범위, 0.333° 간격의 811개 data 를 1° 간격의 361개 data 로 필터링
+        sub_scan = np.array(scan.ranges[0:810 + 1:3])
+        self.sub_scan = np.where(np.array(sub_scan >= self.max_dis, self.max_dis, sub_scan))
 
     def get_steer(self):
-        self.sub_scan[self.sub_scan == 'inf'] = self.max_dis  # 최대 측정 거리를 넘어가 값이 'inf' 로 들어올 때 max_dis 로 변환
-        r_data, l_data = self.sub_scan[55:75 + 1:3], self.sub_scan[195:215 + 1:3]  # 정면 기준 좌우 50° ~ 80° 범위 거리 데이터
-        r_avg, l_avg = sum(r_data) / len(r_data), sum(l_data) / len(l_data)  # 50° ~ 80° 범위의 거리 값들의 평균
-        steer = ((r_avg - l_avg) / (self.tunnel_width - self.width)) * 22 * 1.1  # normalization 후 steer 로 변환, 1.1은 가중치
-        return max(min(steer, 22), -22)
+        # self.sub_scan[self.sub_scan == 'inf'] = self.max_dis  # 최대 측정 거리를 넘어가 값이 'inf' 로 들어올 때 max_dis 로 변환
+        r_data, l_data = self.sub_scan[55:75+1:3], self.sub_scan[195:215+1:3]  # 정면 0° 기준 좌우 60° ~ 80° 범위 거리 데이터
+        r_avg, l_avg = sum(r_data) / len(r_data), sum(l_data) / len(l_data)  # 60° ~ 80° 범위의 거리 값들의 평균
+        steer = ((r_avg - l_avg) / (self.tunnel_width - self.width)) * 22 * 1.1  # -1 ~ 1 로 normalization 후 steer 로 변환, 1.1 은 가중치
+        return np.clip(steer, -22, 22)
 
 
 class PublishToErp:
