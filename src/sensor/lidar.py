221f97cd9eef1377aa192b601008a830de488280
choiyoonji <cyj21c6352@gmail.com>
2023-04-23 22:02:53 +0900

    tf_tm_yd 제거

--- src/sensor/lidar.py
+++ src/sensor/lidar.py
@@ -27,32 +27,6 @@ class lidar:
                 obs_y = -scan_data_for_search[i]*cos(np.deg2rad(float(i)/resolution))
                 self.obs_xy = np.append(self.obs_xy, [np.dot(T, np.transpose([obs_x, obs_y, 1]))], axis=0)
         self.obs_xy[:,2] = 0
-        
-        return  self.obs_xy
-        
-    def tf_tm_yd(self, scan, x , y, heading) :
-        self.obs_xy = np.empty((1, 3))
-        resolution = 2
-        T = [[cos(heading), -sin(heading), x], \
-             [sin(heading),  cos(heading), y], \
-             [      0     ,      0       , 1]]   
-        # yd 라이다의 감지 범위 및 분해능에서 똑같이 -90~90도의 감지값 추출
-        last_scan_data = scan
-        print("range:",len(scan))
-        scan_data_for_search = []
-        for i in range(180, 500):
-            #print(i)
-            scan_data_for_search.append(last_scan_data[i])
-            # 적절하게 감지된 값을 추출해서 저장함, 값이 너무큰거랑 너무 작은거 패스
-            if np.isinf(scan_data_for_search[i-180]) or scan_data_for_search[i-180] < 0.1 or scan_data_for_search[i-180] > 15:
-                pass
-            elif scan_data_for_search[i-180] <= 15:
-                obs_x = -scan_data_for_search[i-180]*sin(np.deg2rad(float(i)/resolution)+90) 
-                obs_y = -scan_data_for_search[i-180]*cos(np.deg2rad(float(i)/resolution)+90)
-                self.obs_xy = np.append(self.obs_xy, [np.dot(T, np.transpose([obs_x, obs_y, 1]))], axis=0)
-        self.obs_xy[:,2] = 0
-        
-        return  self.obs_xy
     
     def clean(self):
         self.code = np.array(self.obs_xy)
